c59838acc5d7bf4f369c2240aadc4f6a42331f37
Oswald Botchway <ennzygh@gmail.com>
2026-04-07 12:02:13 -0500

    Initial Commit

---
+++ BroadcastHQ/BroadcastHQApp.swift
@@ -0,0 +1,17 @@
+//
+//  BroadcastHQApp.swift
+//  BroadcastHQ
+//
+//  Created by Oswald Botchway on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct BroadcastHQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BroadcastHQTests/BroadcastHQTests.swift
@@ -0,0 +1,19 @@
+//
+//  BroadcastHQTests.swift
+//  BroadcastHQTests
+//
+//  Created by Oswald Botchway on 4/7/26.
+//
+
+import Testing
+@testable import BroadcastHQ
+
+struct BroadcastHQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
